0513e967ac4b4acdbceb9e8e65210ee39fb8613c
zubeetroot <zuzanna.bak@beetroot.se>
2024-08-08 13:09:26 +0200

    Add files via upload

---
+++ lesson0/homeworks/task1a.py
@@ -0,0 +1,10 @@
+#task1a.py = the new module which is going to be imported to task1b.py
+
+def function_in_first_module(name):
+	print("This function displays a name: " + name)
+print(function_in_first_module("Abc"))
+
+
+
+
+#11111test
